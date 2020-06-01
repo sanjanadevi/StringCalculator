6949482e7909d56071ed93a46516e7d8d943f9b4
sanjanadevi_p <sanjanadevi_p@solartis.com>
2020-06-01 14:58:26 +0530

    String Calculator TDD 1

--- src/main/java/com/stringCalc/Calculator.java
+++ src/main/java/com/stringCalc/Calculator.java
@@ -1,13 +1,15 @@
 package com.stringCalc;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
 	public static Integer add(String integer) {
 		if (integer.isEmpty())
 			return 0;
-		else if (integer.contains(",")) {
+		else if (integer.contains(",") || integer.contains("//")) {
 			String[] numbers = extractInteger(integer);
 			return sumOfStringNumbers(numbers);
 		} else
@@ -15,7 +17,18 @@ public class Calculator {
 	}
 
 	private static String[] extractInteger(String integer) {
+		if(integer.startsWith("//")) {
+			return splitUsingcustomDelimiter(integer);
+		}
 		return integer.split(",|\n");
+	}
+
+	private static String[] splitUsingcustomDelimiter(String integer) {
+		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(integer);
+		matcher.matches();
+		String delimiter = matcher.group(1);
+		String numbers = matcher.group(2);
+		return numbers.split(delimiter);
 	}
 
 	private static int sumOfStringNumbers(String[] numbers) {
