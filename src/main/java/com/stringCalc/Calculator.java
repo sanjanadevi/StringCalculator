eea5caee51414882c919cdd0422f573e6d345ce6
sanjanadevi_p <sanjanadevi_p@solartis.com>
2020-06-01 14:28:40 +0530

    String Calculator TDD 1

--- src/main/java/com/stringCalc/Calculator.java
+++ src/main/java/com/stringCalc/Calculator.java
@@ -4,9 +4,12 @@ public class Calculator {
 
 	public static Integer add(String integer) {
 		// TODO Auto-generated method stub
-		if(integer.isEmpty())
+		if (integer.isEmpty())
 			return 0;
-		else
+		else if (integer.contains(",")) {
+			String[] numbers = integer.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		} else
 			return Integer.parseInt(integer);
 	}
 
